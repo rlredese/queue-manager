bdad30aec35b309e406a2db72d5ec1706def8e5c
Rodrigo Lipert <>
2024-03-08 20:11:30 -0300

    Alteração do modelo de aplicacao para eecução no Windows

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from time import sleep
+import requests
+import json
+from config import settings
+import pika, sys, os
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='process_queue',durable=True)
+
+    def callback(ch, method, properties, body):
+        try:
+            method_frame = channel.basic_get(queue = 'process_queue')        
+            message = json.loads(body)
+            rpa_id_image = message.get("id_apirpa")
+            #env_file_path = message.get("file_path")
+            env =""
+            image = os.system(f"docker image ls {rpa_id_image}".format(rpa_id_image))
+            os.system("docker run -d -v {volume} -e {env} {image}".format(volume=env_file_path,env=env,image=image))
+            print(f" [x] Received {body}")
+            channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+        except Exception as error:
+            return error
+        
+    channel.basic_consume(queue='process_queue', on_message_callback=callback, auto_ack=False)
+
+    print(' [*] Start working, waiting for messages!')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        #try:
+            #sys.exit(0)
+        #except SystemExit:
+            #os._exit(0)
